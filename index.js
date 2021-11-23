98f9eebfa0888fdf467a42c1ebd00e6550b4a12c
William <wgepeden@gmail.com>
2021-11-22 20:09:30 -0500

    commit tree 6f27eb7cf10b19f5312a7726799b8bb262fb6889

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) return "This one is on me!";
+  else if (distance > 2500) return "No can do.";
+  else if (distance > 2000) return "I will gladly take your thirty bucks.";
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let reply
+  switch(tip) {
+    case 'generous':
+      reply = 'Thank you so much.';
+      return reply;
+      break;
+    case 'not as generous':
+      reply = 'Thank you.';
+      return reply;
+      break;
+    default:
+      reply = "Bye.";
+      return reply;
+  }
 }
